67309319b5cdc367b9f662eebd755f93b870d527
Nakul Sharma <nakulsharma06@gmail.com>
2015-11-26 01:24:44 -0800

    minor changes

--- src/main/java/edu/sjsu/cmpe275/project/controller/UserController.java
+++ src/main/java/edu/sjsu/cmpe275/project/controller/UserController.java
@@ -1,59 +1,89 @@
 package edu.sjsu.cmpe275.project.controller;
 
 import edu.sjsu.cmpe275.project.exception.BadRequestException;
+import edu.sjsu.cmpe275.project.model.Pages;
 import edu.sjsu.cmpe275.project.model.User;
 import edu.sjsu.cmpe275.project.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 /**
  * @author Naks
- * Controller class to handle User APIs.
+ *         Controller class to handle User APIs.
  */
 @Controller
 public class UserController {
 
+    private static final String userSession = "userDetails";
     @Autowired
     IUserService userService;
-
     private HttpSession session = null;
 
-    private static final String signupPage = "signup";
-    private static final String errorPage = "error";
-    private static final String loginPage = "login";
-    private static final String homePage = "home";
-    private static final String userSession = "userDetails";
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String showLogin(User user) {
+        return "login";
+    }
 
-
-    @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public String signUp(
-            @ModelAttribute(value="user") User user, HttpServletRequest request) {
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(@ModelAttribute(value = "user") User user, BindingResult results, HttpServletRequest request, Model model) {
         try {
-            if (user.getName() != null && user.getEmail() != null && user.getPassword() != null) {
-                user = this.userService.createUser(user.getName(), user.getEmail(), user.getPassword());
+            if (results.hasErrors())
+                throw new Exception("Login Error");
+            if (user.getEmail() != null && user.getPassword() != null) {
                 session = request.getSession();
-                session.setAttribute(userSession, user);
-                return homePage;
+                user = this.userService.verifyCredentials(user.getEmail(), user.getPassword());
+                if (user != null) {
+                    session.setAttribute(userSession, user);
+                    return Pages.home.toString();
+                } else
+                    throw new BadRequestException("Password is incorrect!");
             } else
-                    return errorPage;
+                throw new BadRequestException(request.getRequestURI(), "User does not exist.");
 
-        } catch (DataIntegrityViolationException e) {
-            return errorPage;
+        } catch (BadRequestException e) {
+            model.addAttribute("user", new User());
+            model.addAttribute("loginError", true);
+            model.addAttribute("badException", e);
+            return Pages.login.toString();
         } catch (Exception e) {
-            return errorPage;
+            model.addAttribute("exception", e);
+            return Pages.error.toString();
         }
     }
 
     @RequestMapping(value = "/signup", method = RequestMethod.GET)
     public String showSignUp(User user) {
-        return signupPage;
+        return Pages.signup.toString();
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public String signUp(
+            @ModelAttribute(value = "user") User user, HttpServletRequest request, Model model) {
+        try {
+            if (!user.getName().isEmpty() && !user.getEmail().isEmpty() && !user.getPassword().isEmpty()) {
+                user = this.userService.createUser(user.getName(), user.getEmail(), user.getPassword());
+                session = request.getSession();
+                session.setAttribute(userSession, user);
+                return Pages.home.toString();
+            } else
+                throw new BadRequestException("Required fields are missing", HttpStatus.BAD_REQUEST.value(), "mandatory");
+        } catch (BadRequestException e) {
+            model.addAttribute("signupError", true);
+            model.addAttribute("badException", e);
+            return Pages.signup.toString();
+        } catch (Exception e) {
+            model.addAttribute("exception", e);
+            return Pages.error.toString();
+        }
     }
 
     /*@RequestMapping(value="/user", method = RequestMethod.GET)
@@ -67,37 +97,5 @@ public class UserController {
       return "error";
     }*/
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String showLogin(User user) {
-        return "login";
-    }
 
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String login(@ModelAttribute(value = "user") User user, BindingResult results, HttpServletRequest request, Model model) {
-        try {
-            if(results.hasErrors())
-                throw new Exception("Login Error");
-            if (user.getEmail() != null && user.getPassword() != null) {
-                session=request.getSession();
-                user = this.userService.verifyCredentials(user.getEmail(), user.getPassword());
-                if(user!=null) {
-                    session.setAttribute(userSession, user);
-                    return homePage;
-                }
-                else
-                    throw new BadRequestException("Password is incorrect!");
-            } else
-                throw new BadRequestException(request.getRequestURI(),"User does not exist.");
-
-        } catch (BadRequestException e) {
-            model.addAttribute("user", new User());
-            model.addAttribute("loginError", true);
-            model.addAttribute("badException",e);
-            return loginPage;
-        }
-        catch (Exception e){
-            model.addAttribute("exception",e);
-            return errorPage;
-        }
-    }
 }
