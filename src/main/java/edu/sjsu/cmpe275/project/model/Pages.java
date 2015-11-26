0fafaac0b6a237c3c62fbd35f5e773e012b0796d
Nakul Sharma <nakulsharma06@gmail.com>
2015-11-26 01:26:41 -0800

    minor changes

---
+++ src/main/java/edu/sjsu/cmpe275/project/model/Pages.java
@@ -0,0 +1,9 @@
+package edu.sjsu.cmpe275.project.model;
+
+/**
+ * @author Naks
+ * Enumerator to define name of HTML pages.
+ */
+public enum Pages {
+    project,error,login,home,projects,signup
+}
